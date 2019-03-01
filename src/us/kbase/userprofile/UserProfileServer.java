d37ea6d97d0383be2eb4e12d8d7ff8f4d710d9b2
Gavin Price <gaprice@lbl.gov>
2019-03-01 12:23:24 -0800

    Fix the release notes and version

--- src/us/kbase/userprofile/UserProfileServer.java
+++ src/us/kbase/userprofile/UserProfileServer.java
@@ -30,12 +30,15 @@ import org.ini4j.Ini;
  */
 public class UserProfileServer extends JsonServerServlet {
     private static final long serialVersionUID = 1L;
-    private static final String version = "0.2.0";
-    private static final String gitUrl = "git@github.com:kbase/user_profile";
-    private static final String gitCommitHash = "28d01844f1e20012708bf6ce4c66db4ac1f5778f";
+    private static final String version = "0.2.1";
+    private static final String gitUrl = "https://github.com/kbase/user_profile";
+    // the git commmit hash hasn't been updated in 2 years as of 3/1/19 and there doesn't appear
+    // to be a mechanism to update it correctly, as it needs to be SDK compiled from the 
+    // current commit and then built into a server. Any checked in commit must be wrong.
+    private static final String gitCommitHash = "";
 
     //BEGIN_CLASS_HEADER
-    public static final String VERSION = "0.2.0";
+    public static final String VERSION = "0.2.1";
     
     public static final String SYS_PROP_KB_DEPLOYMENT_CONFIG = "KB_DEPLOYMENT_CONFIG";
     public static final String SERVICE_DEPLOYMENT_NAME = "UserProfile";
